3fe5eb45a955a978345f5725fa8fae3629a89641
shahnawaz <sa467546@gmail.com>
2025-07-29 15:04:45 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DEmo_Web_App_Hello_World.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();//We will use for rendering a HTMl page 
+        }
+    }
+}
